3090182858cbbc52a9dfa6b389f850c13169e5a3
Sarah B <smblair87@gmail.com>
2019-03-14 20:38:18 -0700

    Added reset.css code, base html template, and pseudocode for javascript game

---
+++ assets/javascript/game.js
@@ -0,0 +1,64 @@
+//Global variables
+
+// Sets random number
+var randomNumber = [];
+
+// Resets game
+var pauseGame = false
+
+// Array for wins
+var wins = [];
+
+// Array for losses
+var losses = [];
+
+// Array for total score
+var totalScore
+
+// Crystals
+var crystals = [];
+
+// Assigns specific value to crystals
+var crystalValues
+
+
+//============================================================================
+
+// Game code
+
+// Function to reset game
+resetGame()
+
+// .click Function to set up onclick event for crystals
+
+
+// Makes sure key pressed is number
+
+
+// Resets game once user's total score matches random number OR user goes over random number and loses
+function resetGame() {
+    randomNumber = totalScore
+    pauseGame = false
+
+// Gets a new random number
+
+
+// Resets crystal numbers
+    crystals = [];
+    crystalValues = [];
+
+    // For loop to reset crystal values
+    
+
+// Function to update the DOM
+    updateDisplay()
+}
+
+// Updates HTML to show random number, wins, loses, and total score
+
+//This is the setter of the innerHTML property in jQuery
+$('#regTitle').html('Hello World');
+
+//This is the getter of the innerHTML property in jQuery
+var helloWorld = $('#regTitle').html();
+
